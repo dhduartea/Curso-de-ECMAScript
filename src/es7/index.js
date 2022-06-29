ae3c67378ef0cc7cde66106416e1070838fe84b2
David Duarte <dhduartea@gmail.com>
2022-06-29 16:39:35 -0500

    es7 terminado

---
+++ src/es7/index.js
@@ -0,0 +1,22 @@
+//es7
+
+//EJ1-includes
+
+let numbers=[1,2,3,4,5,6,7];
+
+if (numbers.includes(7)) {
+    console.log("Se encuentra el valor");
+} else {
+    console.log("no se encuentra el valor");
+}
+
+//EJ2-**
+
+let base=4;
+let exponente=3;
+let result =base**exponente;
+console.log(result);
+
+base=64
+result=base**(1/exponente);//(Raiz)
+console.log(result.toFixed(2));
